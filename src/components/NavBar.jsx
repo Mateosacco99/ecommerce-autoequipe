b6f494b2e5699caccb115778179cff974338417f
Mateosacco99 <mateosacco99@gmail.com>
2026-01-21 21:36:08 -0300

    Feat: BotonesGenericos en NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,16 @@
 import styles from '../styles/navBar.module.scss';
 import Cart from './Cart';
 import AutoEquipe from '../assets/AutoEquipe.png';
+import { BotonGenerico } from './BotonGenerico';
 
 const NavBar = () => {
     return (
         <nav className={styles['nav-container']}>
             <h1><img className={styles.logo} src={AutoEquipe} alt="AutoEquipe Logo" /></h1>
-            <a href="#" className={styles['nav-item']}>Home</a>
-            <a href="#" className={styles['nav-item']}>Products</a>
-            <a href="#" className={styles['nav-item']}>About</a>
-            <a href="#" className={styles['nav-item']}>Contact</a>
+            <BotonGenerico href="#" tipo="primario">Inicio</BotonGenerico>
+            <BotonGenerico href="#" tipo="primario">Productos</BotonGenerico>
+            <BotonGenerico href="#" tipo="primario">Sobre Nosotros</BotonGenerico>
+            <BotonGenerico href="#" tipo="primario">Contacto</BotonGenerico>
             <Cart />
         </nav>
     );
