b6b0a1e0bcd4fa440f3b770052db71ebd610a38c
jsanmarco <jsanmarco@americavirtualsa.com>
2026-01-28 18:26:55 -0300

    Feat: Async Mock data

---
+++ src/mock/AsyncMock.jsx
@@ -0,0 +1,29 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Producto 1",
+        descripcion: "Descripción del Producto 1",
+        stock: 10,
+        precio: 100,
+        categoria: 'categoria 1',
+        imagen: 'url de imagen'
+    },
+    {
+        id: 2,
+        nombre: "Producto 2",
+        descripcion: "Descripción del Producto 2",
+        stock: 10,
+        precio: 100,
+        categoria: 'categoria 2',
+        imagen: 'url de imagen'
+    },
+    {
+        id: 3,
+        nombre: "Producto 3",
+        descripcion: "Descripción del Producto 3",
+        stock: 10,
+        precio: 100,
+        categoria: 'categoria 3',
+        imagen: 'url de imagen'
+    },
+];
